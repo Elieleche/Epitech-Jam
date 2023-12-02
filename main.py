d80c3b2430a35c89100aa7f036090b81300ad973
laybraid <clement.loeuillet@epitech.eu>
2023-12-02 17:54:33 +0100

    feat: add puzzle game

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from morpion import MorpionGame
 from connect_4 import Connect4
+from puzzle import Puzzle
 
 if __name__ == "__main__":
     game = MorpionGame()
     game.run()
 
     game = Connect4()
+    game.run()
+
+    game = Puzzle()
     game.run()
--- puzzle.py
+++ puzzle.py
@@ -3,65 +3,86 @@ import sys
 import os
 import random
 
-pygame.init()
 
-WIDTH, HEIGHT = 300, 300
-ROWS, COLS = 3, 3
-TILE_SIZE = WIDTH // COLS
+class PuzzleGame:
+    def __init__(self, width=300, height=300, rows=3, cols=3, puzzles_folder="puzzles"):
+        self.width = width
+        self.height = height
+        self.rows = rows
+        self.cols = cols
+        self.tile_size = self.width // self.cols
+        self.puzzles_folder = puzzles_folder
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Puzzle for EPITECH JAM 2023")
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Jeu de Puzzle")
+        self.load_random_image()
+        self.create_pieces()
+        self.shuffle_pieces()
+        self.create_piece_positions()
 
-puzzles_folder = "puzzles"
-image_files = [f for f in os.listdir(puzzles_folder) if os.path.isfile(os.path.join(puzzles_folder, f))]
-random_image = os.path.join(puzzles_folder, random.choice(image_files))
+        self.selected_piece = None
+        self.selected_piece_index = None
 
-image = pygame.image.load(random_image)
-image = pygame.transform.scale(image, (WIDTH, HEIGHT))
+    def load_random_image(self):
+        image_files = [f for f in os.listdir(self.puzzles_folder) if
+                       os.path.isfile(os.path.join(self.puzzles_folder, f))]
+        random_image = os.path.join(self.puzzles_folder, random.choice(image_files))
+        self.image = pygame.image.load(random_image)
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
-pieces = []
-for row in range(ROWS):
-    for col in range(COLS):
-        piece = image.subsurface(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE)
-        pieces.append(piece)
+    def create_pieces(self):
+        self.pieces = []
+        for row in range(self.rows):
+            for col in range(self.cols):
+                piece = self.image.subsurface(col * self.tile_size, row * self.tile_size, self.tile_size,
+                                              self.tile_size)
+                self.pieces.append(piece)
 
-original_pieces = pieces.copy()
+        self.original_pieces = self.pieces.copy()
 
-random.shuffle(pieces)
+    def shuffle_pieces(self):
+        random.shuffle(self.pieces)
 
-piece_positions = [(col * TILE_SIZE, row * TILE_SIZE) for row in range(ROWS) for col in range(COLS)]
+    def create_piece_positions(self):
+        self.piece_positions = [(col * self.tile_size, row * self.tile_size) for row in range(self.rows) for col in
+                                range(self.cols)]
 
-selected_piece = None
-selected_piece_index = None
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                self.handle_mouse_click(event.pos)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    def handle_mouse_click(self, pos):
+        mouse_x, mouse_y = pos
+        clicked_row = mouse_y // self.tile_size
+        clicked_col = mouse_x // self.tile_size
+
+        if self.selected_piece is None:
+            self.selected_piece = self.pieces[clicked_row * self.cols + clicked_col]
+            self.selected_piece_index = clicked_row * self.cols + clicked_col
+        else:
+            self.pieces[self.selected_piece_index], self.pieces[clicked_row * self.cols + clicked_col] = \
+                self.pieces[clicked_row * self.cols + clicked_col], self.selected_piece
+            self.selected_piece = None
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        for i, piece in enumerate(self.pieces):
+            row, col = i // self.cols, i % self.cols
+            self.screen.blit(piece, self.piece_positions[i])
+
+        if self.pieces == self.original_pieces:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            mouse_x, mouse_y = event.pos
-            clicked_row = mouse_y // TILE_SIZE
-            clicked_col = mouse_x // TILE_SIZE
 
-            if selected_piece is None:
-                selected_piece = pieces[clicked_row * COLS + clicked_col]
-                selected_piece_index = clicked_row * COLS + clicked_col
-            else:
-                pieces[selected_piece_index], pieces[clicked_row * COLS + clicked_col] = \
-                    pieces[clicked_row * COLS + clicked_col], selected_piece
-                selected_piece = None
+        pygame.display.flip()
 
-    screen.fill(WHITE)
-    for i, piece in enumerate(pieces):
-        row, col = i // COLS, i % COLS
-        screen.blit(piece, piece_positions[i])
-
-    if pieces == original_pieces:
-        pygame.quit()
-        sys.exit()
-
-    pygame.display.flip()
+    def run(self):
+        while True:
+            self.handle_events()
+            self.draw()
